62367b61ea57dda311b417a19179faafdafc6b9c
Alexis Mena <alexismenanieves@hotmail.com>
2024-03-27 22:43:37 -0500

    Use base model for pydantic

--- app.py
+++ app.py
@@ -1,39 +1,32 @@
-# Load libraries
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
-import numpy as np
-import joblib
-import logging
+# Import libraries
+import pickle
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from contextlib import asynccontextmanager
+from pydantic import BaseModel
+from magnum import Magnum
+from typing import List
 
-# Load model and data vectorizer
-model = joblib.load('model.pkl')
+# Load the model
+model_components = pickle.load(open("model_components.pkl", "rb"))
+model = model_components["model"]
+extras = model_components["extras"]
 
-# Create flask API
-app = Flask(__name__)
-API = Api(app)
+# Input data validation
+class Input(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
 
-# Create parser
-class Predict(Resource):
-    @staticmethod
-    def post():
-        parser = reqparse.RequestParser()
-        parser.add_argument('sep_l', type=float)
-        parser.add_argument('sep_w', type=float)
-        parser.add_argument('pet_l', type=float)
-        parser.add_argument('pet_w', type=float)
+class ModelInputs(BaseModel):
+    inputs: List[Input]
+    def return_dict_inputs(cls):
+        return [dict(input) for input in cls.all]
 
-        args = parser.parse_args()
-        #X_new = np.fromiter(args.values(), dtype=float)
-        X_new = list(args.values())
-        # Interestingly, it returns a numpy array, so to extract
-        # use .item()
-        out = {'Prediction': str(model.predict([X_new])[0])}
+ml_models = {}
 
-        return out, 200
+@async
 
-# Define an endpoint
-API.add_resource(Predict, '/predict')
-
-# Run the flask application
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=9696)
+@app.get
